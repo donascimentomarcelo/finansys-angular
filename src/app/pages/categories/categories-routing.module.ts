ed1d4e5006980539bb6cfd38e51b38a599e7b9c8
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-11-23 22:17:07 -0200

    Criando rotas lazy loading para categories e suas sub rotas.

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CategoryListComponent },
+  { path: ':id', component: CategoryFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class CategoriesRoutingModule { }
