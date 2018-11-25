bb5e843e03677f096feec3c3239d420afe36aa9b
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-11-24 23:44:19 -0200

    Criando o modelo de categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,9 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/Category';
 
 export class InMemoryDatabase implements InMemoryDbService {
 
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: 'Teste 1', description: 'blablabla'},
             { id: 2, name: 'Teste 2', description: 'blablabla'},
             { id: 3, name: 'Teste 3', description: 'blablabla'},
