482b60741c75ded7f65ee83075a13b8232050520
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-11-26 22:17:10 -0200

    Listando categorias e excluindo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from './in-memory-database';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { InMemoryDatabase } from './in-memory-database';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/Category';
+import { element } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  public categories: Category[] = [];
+  constructor(
+    private categoryService: CategoryService,
+  ) { }
 
   ngOnInit() {
+    this.carregaCategorias();
+  }
+
+  public carregaCategorias(): void {
+    this.categoryService.getAll()
+      .subscribe((resp) => {
+        this.categories = resp;
+      });
+  }
+
+  public delete(category: Category): void {
+    this.categoryService.delete(category.id)
+      .subscribe(() => {
+        this.categories = this.categories.filter(element => element !== category);
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
