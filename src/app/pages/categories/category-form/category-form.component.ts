49e9cb828ecb3dda95c2edc58e4bf776c49ad01e
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-11-28 23:56:12 -0200

    Criando formulario reativo e outros metodos basicos de categoria

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Category } from './../shared/Category';
+import { CategoryService } from './../shared/category.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm = false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
 
+  ngAfterContentChecked(): void {
+    this.setTitle();
+  }
+
+  private setCurrentAction(): void {
+    if (this.activatedRoute.url[0].path === 'new') {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit';
+    }
+  }
+
+  private buildCategoryForm(): void {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory(): void {
+    if (this.currentAction === 'edit') {
+
+      this.activatedRoute.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get('id')))
+      ).subscribe((resp) => {
+        this.category = resp;
+        this.categoryForm.patchValue(resp);
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  private setTitle(): void {
+    if (this.currentAction === 'new') {
+      this.pageTitle = 'Criando categoria';
+    } else {
+      const categoryName = this.category.name || '';
+      this.pageTitle = 'Editando categoria :' + categoryName;
+    }
+  }
 }
