8f2d45d4308837545e145229ba2331021a8579ab
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-12-03 22:31:14 -0200

    Configurando o shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ],
-  declarations: []
+  declarations: [],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+  ]
 })
 export class SharedModule { }
