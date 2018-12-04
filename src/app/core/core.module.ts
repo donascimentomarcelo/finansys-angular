cb21492d394e596445077ead9a6bf2302fbad8f6
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-12-03 22:05:27 -0200

    Configurando o core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from '../in-memory-database';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
-  declarations: []
+  declarations: [],
+  exports: [
+    // Modulos compartilhados
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ]
 })
 export class CoreModule { }
