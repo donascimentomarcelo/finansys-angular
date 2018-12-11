0727b8ccfa8668aa19ec2af12e5a35eaed629a0e
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-12-10 22:29:56 -0200

    Criando primeira parte do baseResourceFormComponent

---
+++ src/app/shared/components/base-resource-form/base-resource-form.component.ts
@@ -0,0 +1,44 @@
+import { OnInit, AfterContentChecked, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs/operators';
+import toastr from 'toastr';
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
+import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel> implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  resourceForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm = false;
+
+  protected activatedRoute: ActivatedRoute;
+  protected router: Router;
+  protected formBuilder: FormBuilder;
+
+  constructor(
+    protected injector: Injector,
+    public resource: T,
+    protected resourceService: BaseResourceService<T>,
+    protected jsonDataToResourceFn: (jsonData) => T
+
+  ) {
+    this.activatedRoute = this.injector.get(ActivatedRoute);
+    this.router = this.injector.get(Router);
+    this.formBuilder = this.injector.get(FormBuilder);
+   }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterContentChecked(): void {
+
+  }
+
+ 
+
+}
