2e168f5dba2eb6764c1f707f523c30ea3ab62c92
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-12-12 22:43:03 -0200

    Refatorando CategoryListComponent com BaseResourceListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -2,37 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from './../shared/category.service';
 import { Category } from './../shared/Category';
 import { element } from '@angular/core/src/render3/instructions';
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  public categories: Category[] = [];
   constructor(
-    private categoryService: CategoryService,
-  ) { }
-
-  ngOnInit() {
-    this.carregaCategorias();
-  }
-
-  public carregaCategorias(): void {
-    this.categoryService.getAll()
-      .subscribe((resp) => {
-        this.categories = resp;
-      });
-  }
-
-  public delete(category: Category): void {
-    this.categoryService.delete(category.id)
-      .subscribe(() => {
-        this.categories = this.categories.filter(element => element !== category);
-      }, error => {
-        console.log(error);
-      });
+    protected categoryService: CategoryService,
+  ) {
+    super(categoryService);
   }
 
 }
