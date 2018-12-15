f2f9cf66182a968d919f11ced306c6160ffad347
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-12-14 23:17:02 -0200

    Utilizando componente app-bread-crumb
    Corrigindo o shared module utilizando o RouterModule para que o componente reconheca o property bind do routerLink.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   imports: [
-    CommonModule,
+  CommonModule,
     ReactiveFormsModule,
+    RouterModule,
   ],
   declarations: [BreadCrumbComponent],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     BreadCrumbComponent,
+    RouterModule,
   ]
 })
 export class SharedModule { }
